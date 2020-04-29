2da3be046aa4daf5b369760b412c9c748f9fe158
mephi42 <mephi42@gmail.com>
2020-04-29 21:58:14 +0200

    Build i686 glibc

--- build.sh
+++ build.sh
@@ -7,13 +7,14 @@ set -ex
 if [ $PLATFORM == x86_64 ] || [ "$1" == "glibc_only" ]; then
     # Output something every 10 minutes or Travis kills the job
     while sleep 9m; do echo -n -e " \b"; done &
-    docker build --rm -t centos-with-vsyscall:latest --cache-from centos-with-vsyscall:latest --target centos-with-vsyscall -f docker/glibc/Dockerfile docker/glibc/
+    docker build --rm -t centos-with-vsyscall32:latest --cache-from centos-with-vsyscall32:latest --target centos-with-vsyscall32 -f docker/glibc/Dockerfile docker/glibc/
+    docker build --rm -t centos-with-vsyscall64:latest --cache-from centos-with-vsyscall32:latest --cache-from centos-with-vsyscall64:latest --target centos-with-vsyscall64 -f docker/glibc/Dockerfile docker/glibc/
     # Killing background sleep loop
     kill %1
     if [ "$1" == "glibc_only" ]; then
         exit 0
     fi
-    docker build --rm -t quay.io/pypa/manylinux2010_centos-6-no-vsyscall --cache-from quay.io/pypa/manylinux2010_centos-6-no-vsyscall:latest --cache-from centos-with-vsyscall:latest -f docker/glibc/Dockerfile docker/glibc/
+    docker build --rm -t quay.io/pypa/manylinux2010_centos-6-no-vsyscall --cache-from quay.io/pypa/manylinux2010_centos-6-no-vsyscall:latest --cache-from centos-with-vsyscall32:latest --cache-from centos-with-vsyscall64:latest -f docker/glibc/Dockerfile docker/glibc/
 fi
 
 docker build --rm -t quay.io/pypa/manylinux2010_$PLATFORM:$TRAVIS_COMMIT -f docker/Dockerfile-$PLATFORM docker/
